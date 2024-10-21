16b22c9767ce04bc4400ff3b2a488000996dd46b
Rajkumar <46551565+Rajpopzein@users.noreply.github.com>
2024-10-21 14:44:14 +0530

    Add files via upload
    add

--- src/component/activityComponent/ProfileComponent.jsx
+++ src/component/activityComponent/ProfileComponent.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Card, CardContent, Typography, Avatar } from "@mui/material";
 import "./ProfileComponent.css";
 
-const ProfileComponent = () => {
+const ProfileComponent = (data) => {
+  console.log("data", data);
   return (
     <Card className="profileCardComp">
       <CardContent>
         <div className="activity-card-header">
           <Avatar
             src="https://posterjack.ca/cdn/shop/articles/Portriat_Photography_Composition_Tips.jpg?v=1563409841&width=1500"
-            alt=""
+            alt={data?.data?.data?.name}
             ravan
           />
           <div style={{ marginTop: "0.4rem" }}>
@@ -18,14 +19,14 @@ const ProfileComponent = () => {
               variant="subtitle2"
               component="div"
             >
-              Rajkumar
+              {data?.data?.data?.name}
             </Typography>
             <Typography
               className="username-comp"
               variant="subtitle3"
               component="div"
             >
-              online
+              {data?.data?.data?.email}
             </Typography>
           </div>
         </div>
--- src/pages/Dashboard/DashBoard.jsx
+++ src/pages/Dashboard/DashBoard.jsx
@@ -11,64 +11,98 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import { jwtDecode } from "jwt-decode";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Button } from "@mui/material";
+import { toast } from "react-toastify";
 
 const DashBoard = () => {
   const token = localStorage.getItem("token");
   const decodedToken = jwtDecode(token);
   const [alluser, setAlluser] = useState([]);
   const [allPosts, setAllpost] = useState([]);
-  const [newPost, setNewpost] = useState({
-    userId: decodedToken.data.id,
-    content: "",
-    visibility: "public",
-  });
+  const [allUsers, setAllusers] = useState([]);
+
+  const names = ["public", "friends"];
 
   const postdata = async () => {
     const data = await axios.get(
       `http://54.145.12.92:8000/api/posts/${decodedToken?.data?.id}/feed`
     );
-    console.log(data?.data);
     setAllpost(data?.data);
   };
 
-  useEffect(() => {
-    const userData = async () => {
-      const data = await axios.get(
-        `http://54.145.12.92:8000/api/users/all-users/${decodedToken?.data?.id}`
-      );
+  const getalluser = async () => {
+    const data = await axios.get(
+      `http://54.145.12.92:8000/api/users/${decodedToken?.data?.id}/exclude-self-friends`
+    );
+    setAllusers(data?.data);
+  };
+  const userData = async () => {
+    const data = await axios.get(
+      `http://54.145.12.92:8000/api/users/${decodedToken?.data?.id}/friends`
+    );
+    console.log(data, "ddddd");
+    setAlluser(data?.data);
+  };
 
-      setAlluser(data?.data);
-    };
+  useEffect(() => {
+    getalluser();
     postdata();
     userData();
   }, []);
 
-  const handleNewpostContent = (e) => {
-    setNewpost({ ...newPost, content: e.target.value });
-  };
-
-  const handleSubmitpostContent = async (e) => {
-    e.preventDefault();
-    if (newPost.content.trim() === "") {
-      alert("Please enter a post content");
-      return;
-    }
-    axios
-      .post(
-        `http://54.145.12.92:8000/api/posts/${decodedToken?.data?.id}/create`,
-        newPost
-      )
-      .then((response) => {
-        console.log(response);
-        postdata();
-      });
-
-    setNewpost({
-      userId: decodedToken?.data?.id,
+  // Formik setup
+  const formik = useFormik({
+    initialValues: {
       content: "",
-      visibility: "public",
-    });
+      visibility: [],
+    },
+    validationSchema: Yup.object({
+      content: Yup.string().required("Please enter a post content"),
+      visibility: Yup.array().min(
+        1,
+        "Please select at least one visibility option"
+      ),
+    }),
+    onSubmit: async (values, { resetForm }) => {
+      try {
+        await axios.post(
+          `http://54.145.12.92:8000/api/posts/${decodedToken?.data?.id}/create`,
+          { ...values, userId: decodedToken.data.id }
+        );
+        postdata();
+        toast("Post created successfully");
+        resetForm();
+      } catch (error) {
+        console.error("Error creating post", error);
+      }
+    },
+  });
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [hoverdata, setHoveredData] = useState({});
+
+  const handleaddfrnd = async () => {
+    const value = {
+      friendId: hoverdata._id,
+    };
+    const data = await axios.put(
+      `http://54.145.12.92:8000/api/users/${decodedToken?.data?.id}/add-friend`,
+      value
+    );
+    if (data.status === 200) {
+      toast("Friend added successfully");
+      getalluser();
+      getalluser();
+      userData();
+    }
   };
+
   return (
     <div className="dashboard-main">
       <div className="welcome_note">
@@ -79,15 +113,59 @@ const DashBoard = () => {
             placeholder="search for friends"
             className="search-for-frind"
           />
+          {allUsers.length === 0 && (
+            <p style={{ fontSize: "0.8rem", marginTop: "5px" }}>
+              No friends found
+            </p>
+          )}
+          <List
+            sx={{ width: "100%", bgcolor: "background.paper" }}
+            className="all-user-list"
+          >
+            {allUsers &&
+              allUsers.map((user, index) => (
+                <ListItem
+                  key={index}
+                  onMouseEnter={() => {
+                    setHoveredIndex(index);
+                    setHoveredData(user);
+                  }}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                  sx={{ position: "relative" }} // Ensures button can be positioned correctly
+                >
+                  <ListItemAvatar>
+                    <Avatar src="" alt={user.name} />
+                  </ListItemAvatar>
+                  <ListItemText primary={user.name} secondary={user.email} />
+
+                  {/* Show button on hover */}
+                  {hoveredIndex === index && (
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{
+                        position: "absolute",
+                        right: 0,
+                        backgroundColor: "#000",
+                        width: "100px",
+                      }} // Adjust position as needed
+                      onClick={handleaddfrnd} // Handle add friend action
+                    >
+                      Add Friend
+                    </Button>
+                  )}
+                </ListItem>
+              ))}
+          </List>
         </Card>
       </div>
       <div className="activity_card_section">
         <Card className="postcreation-card">
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <TextField
-              onChange={handleNewpostContent}
-              value={newPost.content}
-              id="outlined-multiline-flexible"
+              onChange={formik.handleChange}
+              value={formik.values.content}
+              name="content"
               label="Add your Feed"
               multiline
               maxRows={4}
@@ -103,32 +181,78 @@ const DashBoard = () => {
                   },
                 },
               }}
+              error={formik.touched.content && Boolean(formik.errors.content)}
+              helperText={formik.touched.content && formik.errors.content}
             />
-            <button onClick={handleSubmitpostContent} className="add-post-btn">
+            <FormControl
+              style={{ marginRight: "1rem", width: "100px" }}
+              error={
+                formik.touched.visibility && Boolean(formik.errors.visibility)
+              }
+            >
+              <Select
+                multiple
+                className="select-selectp"
+                displayEmpty
+                value={formik.values.visibility}
+                onChange={formik.handleChange}
+                name="visibility"
+                input={<OutlinedInput />}
+                renderValue={(selected) => {
+                  if (selected.length === 0) {
+                    return <em>Visibility</em>;
+                  }
+                  return selected.join(", ");
+                }}
+              >
+                <MenuItem disabled value="">
+                  <em>Visibility</em>
+                </MenuItem>
+                {names.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+              {formik.touched.visibility && formik.errors.visibility && (
+                <Typography color="error" variant="caption">
+                  {formik.errors.visibility}
+                </Typography>
+              )}
+            </FormControl>
+            <button type="submit" className="add-post-btn">
               Add Post
             </button>
           </form>
         </Card>
         {allPosts &&
-          allPosts.map((post) => (
+          allPosts.map((post, index) => (
             <ActivityCard
+              key={index}
               username={post.userId.name}
               activityDescription={post.content}
             />
           ))}
       </div>
       <div className="profile">
-        <ProfileComponent />
+        <ProfileComponent data={decodedToken} />
         <Card className="mainuserlistcard">
-          <p style={{ fontWeight: "500", marginBottom: "0.1rem" }}>All Users</p>
+          <p style={{ fontWeight: "500", marginBottom: "0.1rem" }}>Friends</p>
+          {alluser.length === 0 && (
+            <p style={{ fontSize: "0.8rem" }}>No friends found</p>
+          )}
           <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+            {console.log(alluser, "friends")}
             {alluser &&
-              alluser.map((user) => (
-                <ListItem>
+              alluser.map((user, index) => (
+                <ListItem key={index}>
                   <ListItemAvatar>
-                    <Avatar src="" alt={user.name} />
+                    <Avatar src="" alt={user.friendId.name} />
                   </ListItemAvatar>
-                  <ListItemText primary={user.name} secondary="online" />
+                  <ListItemText
+                    primary={user.friendId.name}
+                    secondary={user.friendId.email}
+                  />
                 </ListItem>
               ))}
           </List>
