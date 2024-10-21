07deb339878ef0b396c9e64bf968a2a5e4c9bf4d
Rajkumar <46551565+Rajpopzein@users.noreply.github.com>
2024-10-21 17:51:15 +0530

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,30 +1,55 @@
-import { Route, Router, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation, Navigate } from "react-router-dom";
 import Navbar from "./component/navbar/Navbar.jsx";
 import { lazy, Suspense } from "react";
 import Loader from "./component/loader/Loader.jsx";
 
+
 const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage.jsx"));
-
 const ChatPage = lazy(() => import("./pages/ChatPage/ChatPage.jsx"));
-
 const Signup = lazy(() => import("./pages/SignUpPage/SignupPage.jsx"));
-
 const DashBoard = lazy(() => import("./pages/Dashboard/DashBoard.jsx"));
+
+
+const isAuthenticated = () => !!localStorage.getItem("token");
+
+
+const PrivateRoute = ({ children }) => {
+  return isAuthenticated() ? children : <Navigate to="/" />;
+};
+
+// Public Route Component
+const PublicRoute = ({ children }) => {
+  return !isAuthenticated() ? children : <Navigate to="/DashBoard" />;
+};
 
 const RoutesElement = () => {
   const location = useLocation();
 
   const noNavbarPaths = ["/", "/register"];
-
   const shouldDisplayNavbar = !noNavbarPaths.includes(location.pathname);
+
   return (
     <>
       {shouldDisplayNavbar ? (
         <Navbar>
           <Suspense fallback={<Loader />}>
             <Routes>
-              <Route path="/chat" element={<ChatPage />} />
-              <Route path="/DashBoard" element={<DashBoard />} />
+              <Route
+                path="/chat"
+                element={
+                  <PrivateRoute>
+                    <ChatPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/DashBoard"
+                element={
+                  <PrivateRoute>
+                    <DashBoard />
+                  </PrivateRoute>
+                }
+              />
               <Route path="/contact" element={<h1>Contact</h1>} />
               <Route path="*" element={<h1>Not Found</h1>} />
             </Routes>
@@ -33,8 +58,22 @@ const RoutesElement = () => {
       ) : (
         <Suspense fallback={<Loader />}>
           <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/register" element={<Signup />} />
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <LoginPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute>
+                  <Signup />
+                </PublicRoute>
+              }
+            />
             <Route path="*" element={<h1>Not Found</h1>} />
           </Routes>
         </Suspense>
